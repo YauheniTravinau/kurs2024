3be58e488714a958d020bb2170c244134fc16bb2
YauheniTravinau <128102952+YauheniTravinau@users.noreply.github.com>
2024-01-06 19:44:32 +0100

    ngudalsia

--- incubator/pre-course-hw-js/lesson-02/03.js
+++ incubator/pre-course-hw-js/lesson-02/03.js
@@ -1,8 +1,10 @@
 const giveMeRightNumbers = (value1, value2, maximum) => {
     if (value1 === value2 || value1 <= maximum) {
-        console.log('Success!');
+        return 'Success!';
     } else {
-        console.log('Something wrong!');
+        return 'Something wrong!';
     }
-}
-giveMeRightNumbers(200, 100, 100)
+};
+
+const result = giveMeRightNumbers(200, 100, 100);
+console.log(result);
