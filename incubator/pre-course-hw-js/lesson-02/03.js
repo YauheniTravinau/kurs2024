fc0886426f727c13a4e62d53d7f688129cf61867
YauheniTravinau <128102952+YauheniTravinau@users.noreply.github.com>
2024-01-06 17:41:44 +0100

    ngudalsia2

--- incubator/pre-course-hw-js/lesson-02/03.js
+++ incubator/pre-course-hw-js/lesson-02/03.js
@@ -1,16 +1,10 @@
 console.log('Something wrong!');
 const giveMeRightNumbers = (value1, value2, maximum) => {
-    if (value1 === value2 || value1 >= maximum) {
+    if (value1 === value2 || value1 <= maximum) {
         console.log('Success!');
     } else {
         console.log('Something wrong!');
     }
 }
 
-// Замените числа на фактические значения
-giveMeRightNumbers(5, 5, 10);
-
-// Ваш тестовый код для проверки вывода в консоль
-local_cypress_1.cy.stub(console, 'log').as('consoleLog');
-new Function(this.studentCode)();
-local_cypress_1.cy.get('@consoleLog').should('be.calledOnce');
+giveMeRightNumbers(10, 20, 5);
