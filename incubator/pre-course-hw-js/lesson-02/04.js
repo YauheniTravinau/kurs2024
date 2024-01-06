537942c18defa0e541e21d44ef259ae797d697b0
YauheniTravinau <128102952+YauheniTravinau@users.noreply.github.com>
2024-01-06 20:30:25 +0100

    ngudalsia667

--- incubator/pre-course-hw-js/lesson-02/04.js
+++ incubator/pre-course-hw-js/lesson-02/04.js
@@ -1,8 +1,10 @@
 const giveMeRightCondition = (value1, value2, maximum) => {
     if (value1 === value2 && value1 <= maximum) {
         console.log('Success!');
+        return 'Success!';
     } else {
         console.log('Something wrong!');
+        return 'Something wrong!';
     }
 }
 
