6810272575783340bac0423861e293aa53c1fd29
YauheniTravinau <128102952+YauheniTravinau@users.noreply.github.com>
2024-01-14 10:34:24 +0100

    5566

--- incubator/pre-course-hw-js/lesson-03/01.js
+++ incubator/pre-course-hw-js/lesson-03/01.js
@@ -1,8 +1,8 @@
 const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
 
 function createColorString() {
-    const germanFlagColors = colors.filter(color => color === "черный" || color === "красный" || color === "желтый");
-    return germanFlagColors.join('-');
+    const germanColors = colors.filter(color => color === "черный" || color === "красный" || color === "желтый");
+    return germanColors.join('-');
 }
 
 console.log(createColorString());
