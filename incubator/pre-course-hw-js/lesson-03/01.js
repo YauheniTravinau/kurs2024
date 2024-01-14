99e8313e62f4296a45e5f807bd76b64b1f2fa355
YauheniTravinau <128102952+YauheniTravinau@users.noreply.github.com>
2024-01-14 11:51:07 +0100

    17767

--- incubator/pre-course-hw-js/lesson-03/01.js
+++
@@ -1,8 +0,0 @@
-const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
-
-function createColorString() {
-    const germanFlagColors = colors.filter(color => color === "черный" || color === "красный" || color === "желтый");
-    return germanFlagColors.join('-');
-}
-
-console.log(createColorString());
