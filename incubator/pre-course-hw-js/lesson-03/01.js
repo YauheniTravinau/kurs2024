0ac574d055ed26236f00e4972c63933360b52ccd
YauheniTravinau <128102952+YauheniTravinau@users.noreply.github.com>
2024-01-14 10:58:59 +0100

    5566789

--- incubator/pre-course-hw-js/lesson-03/01.js
+++ incubator/pre-course-hw-js/lesson-03/01.js
@@ -1,7 +1,7 @@
 const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
 
 function createColorString() {
-    return colors.filter(color => color === "черный" || color === "красный" || color === "желтый").join("-");
+    return colors.join('-').split('-').filter(color => color === "черный" || color === "красный" || color === "желтый").join('-');
 }
 
 console.log(createColorString());
